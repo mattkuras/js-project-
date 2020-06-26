28fb8a2121cacc9cd81b177627292ec543e02d49
matt_kuras <mattkuras@gmail.com>
2020-06-26 16:31:13 -0400

    add card tmeplate

---
+++ js-project-backend/models/trip.js
@@ -0,0 +1,11 @@
+class Trip {
+    constructor(id, name, photos, description, season, rating, user_id ){
+      this.id = id
+      this.name = name
+      this.photos = photos
+      this.description = description
+      this.season = season
+      this.rating = rating
+      this.user_id = user_id
+      this.renderTrip()
+    }
