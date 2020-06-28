f037a8427c97278e9bece6512465ec0298cb8e0e
matt_kuras <mattkuras@gmail.com>
2020-06-28 18:58:21 -0400

    add create action and strong params

--- js-project-frontend/app/controllers/trips_controller.rb
+++ js-project-frontend/app/controllers/trips_controller.rb
@@ -3,4 +3,21 @@ class TripsController < ApplicationController
         @trips = Trip.all
         render json: @trips.as_json(include: {activities: {only: [:name, :id]}})
     end
+
+    def create
+
+        @trip = Trip.new(trip_params)
+    byebug
+        if @trip.save
+          render json: @trip#, status: :created, location: @trip
+        else
+          render json: @trip.errors#, status: :unprocessable_entity
+        end
+      end
+
+      private 
+
+      def trip_params
+        params.require(:trip).permit(:name, :rating, :season, :description, :photos, :user_id)
+      end
 end
