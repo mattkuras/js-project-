e3a9f0d82c7ab62cd343fe50150b334d399a86af
matt_kuras <mattkuras@gmail.com>
2020-06-28 18:57:45 -0400

    add event listener for form submit

--- js-project-backend/src/index.js
+++ js-project-backend/src/index.js
@@ -6,7 +6,7 @@
 document.addEventListener("DOMContentLoaded",function(){
   // call our new function in there 
   API.addTrips()
-  
+  document.getElementById('trip-form').addEventListener('submit', API.addTrip)
   // add the form event listener to the domContentLoaded
  // document.getElementById('form').addEventListener('submit', API.addHog)
 })
