9189b3c85ed91f1fa821ebf72ac19edc48c7917a
matt_kuras <mattkuras@gmail.com>
2020-07-01 14:14:04 -0400

    add function to render trip form

--- js-project-backend/models/trip.js
+++ js-project-backend/models/trip.js
@@ -35,6 +35,68 @@ class Trip {
       //     })
       // })
       // }
+     
+      static renderTripForm(){
+        const container = document.getElementById("form")
+        const tripForm = document.createElement("form")
+        tripForm.classList.add("hide")
+        const label = document.createElement('h2')
+        label.innerText = "Make a new trip!!"
+        container.appendChild(label)
+        label.addEventListener("click", () => {
+          console.log("clicked")
+            tripForm.classList.remove("hide")
+        })
+        tripForm.id = "trip-form"
+        container.appendChild(tripForm)
+
+        const nameInput = document.createElement("input")
+        nameInput.name = "name"
+        nameInput.type = "text"
+        nameInput.placeholder = "title"
+        tripForm.appendChild(nameInput)
+
+        const seasonInput = document.createElement("input")
+        seasonInput.name = "season"
+        seasonInput.type = "text"
+        seasonInput.placeholder = "season(s)"
+        tripForm.appendChild(seasonInput)
+
+        const descriptionInput = document.createElement("input")
+        descriptionInput.name = "description"
+        descriptionInput.type = "text"
+        descriptionInput.placeholder = "add a description"
+        tripForm.appendChild(descriptionInput)
+
+        const ratingInput = document.createElement('select')
+        ratingInput.name = "rating"
+
+        for (let i = 1; i < 6; i++){
+          const option = document.createElement('option')
+          option.value = `${i} Star`
+          option.innerText = `${i} Star`
+          ratingInput.appendChild(option) 
+        }
+        tripForm.appendChild(ratingInput)
+
+        const imageInput = document.createElement('input')
+        imageInput.name = "photos"
+        imageInput.type = "text"
+        imageInput.placeholder = "image url"
+        tripForm.appendChild(imageInput)
+
+        const userId = document.createElement('input')
+        userId.type = "text"
+        userId.name = "user_id"
+        const requiredId = document.getElementById("users-id").innerHTML
+        userId.value = requiredId
+        tripForm.appendChild(userId)
+
+        const subButton = document.createElement("input")
+        subButton.type = "submit"
+        subButton.value = "Submit"
+        tripForm.appendChild(subButton)
+      }
 
       deleteTrip(e){
         console.log("deleted")
